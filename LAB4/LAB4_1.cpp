a07d01d0937e73ed783f91f25c2b6cea2bdbe8e1
Ratchanon Panmas <qq.panmas@gmail.com>
2023-09-09 20:32:48 +0700

    LAB4_1

--- LAB4/LAB4_1.cpp
+++ LAB4/LAB4_1.cpp
@@ -35,38 +35,6 @@ int count(vector<int> &coins, int n, int sum, vector<vector<int>> &dp, vector<ve
     return dp[n][sum] = include + exclude;
 }
 
-tuple<int, vector<int>> readFile(string filepath)
-{
-    ifstream testCaseFile(filepath);
-
-    int amount;
-    vector<int> coins;
-
-    if (testCaseFile.is_open())
-    {
-        string line;
-        getline(testCaseFile, line);
-
-        amount = stoi(line);
-
-        getline(testCaseFile, line);
-        stringstream ss(line);
-
-        while (ss.good())
-        {
-            string substr;
-            getline(ss, substr, ' ');
-
-            if (substr != "")
-                coins.push_back(stoi(substr));
-        }
-    }
-    else
-        cout << "Unable to open file";
-
-    return make_tuple(amount, coins);
-}
-
 void displayDenomination(vector<vector<int>> &denomination)
 {
     printf("Denomination: ");
@@ -130,63 +98,49 @@ void displayTable(vector<vector<int>> &dp, vector<int> &coins)
     }
 }
 
-vector<string> getAllTestCaseFile(string folderPath)
-{
-    vector<string> files;
-    for (const auto &entry : filesystem::directory_iterator(folderPath))
-    {
-        files.push_back(entry.path());
-    }
-
-    sort(files.begin(), files.end(), [](const string &a, const string &b)
-         { return stoi(a.substr(a.find_last_of("/\\") + 1).substr(2, 3)) > stoi(b.substr(b.find_last_of("/\\") + 1).substr(2, 3)); });
-
-    return files;
-}
-
 int main()
 {
 
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    vector<string> files = getAllTestCaseFile("./test");
-    // vector<string> files;
-    // files.push_back("./test/4.0.txt");
+    int amount;
+    vector<int> coins;
 
-    while (!files.empty())
+    cin >> amount;
+
+    // read until EOF
+    while (cin.good())
     {
-        string filepath = files.back();
-        files.pop_back();
-
-        printf("File: %s\n", filepath.c_str());
-
-        int amount;
-        vector<int> coins;
-        tie(amount, coins) = readFile(filepath);
-
-        int n = coins.size();
-
-        printf("Amount: %d\n", amount);
-        // printf("Coins: ");
-        // for (int i = 0; i < n; i++)
-        //     printf("%d ", coins[i]);
-        // printf("\n");
-
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
-        vector<vector<int>> denomination;
-        vector<int> temp_denomination;
-
-        auto start = chrono::high_resolution_clock::now();
-        printf("Number of ways: %'d\n", count(coins, n, amount, dp, denomination, temp_denomination));
-        auto end = chrono::high_resolution_clock::now();
-
-        // Display Denomination
-        // displayDenomination(denomination);
-        displayTime(end - start);
-        // displayTable(dp, coins);
-
-        printf("\n");
+        int coin;
+        cin >> coin;
+        coins.push_back(coin);
     }
+
+    int n = coins.size();
+
+    printf("Amount: %d\n", amount);
+    // printf("Coins: ");
+    // for (int i = 0; i < n; i++)
+    //     printf("%d ", coins[i]);
+    // printf("\n");
+
+    vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
+    vector<vector<int>> denomination;
+    vector<int> temp_denomination;
+
+    auto start = chrono::high_resolution_clock::now();
+    int result = count(coins, n, amount, dp, denomination, temp_denomination);
+    auto end = chrono::high_resolution_clock::now();
+
+    printf("Number of ways: %d\n", result);
+
+    // Display Denomination
+    displayDenomination(denomination);
+    displayTime(end - start);
+    // displayTable(dp, coins);
+
+    printf("\n");
+
     return 0;
 }
---
+++ run.sh
@@ -0,0 +1,31 @@
+# Compile file from input path
+
+# Get input path
+input_path=$1
+file_name=$(basename $input_path)
+
+# Output path = input path + file name without extension
+output_path="${input_path%.*}"
+
+# Compile file
+g++-13 --std=c++17 -o $output_path $input_path
+
+# Get test case directory path
+test_case_dir_path=$2
+
+# Sort test case files
+test_case_dir_path=$(echo $test_case_dir_path/* | tr " " "\n" | sort -V | tr "\n" " ")
+
+# Run test cases
+for test_case_path in $test_case_dir_path; do
+    echo "Running test case: $test_case_path"
+
+    LAB=$(echo $test_case_path | cut -d'/' -f2)
+    TEST_CASE=$(basename $test_case_path)
+
+    mkdir -p "$LAB/output"
+
+    output_file_path="$LAB/output/$TEST_CASE"
+    $output_path < $test_case_path > $output_file_path
+
+done
