5fbbffa3ac60b64734a2bae6ab502d6a15db9e0b
Ratchanon Panmas <qq.panmas@gmail.com>
2023-09-09 18:58:41 +0700

    LAB4 but cpp

--- LAB4/LAB4_1.cpp
+++ LAB4/LAB4_1.cpp
@@ -2,11 +2,14 @@
 
 using namespace std;
 
-int count(vector<int> &coins, int n, int sum, vector<vector<int>> &dp)
+int count(vector<int> &coins, int n, int sum, vector<vector<int>> &dp, vector<vector<int>> &denomination, vector<int> &temp_denomination)
 {
     // Base Case
     if (sum == 0)
+    {
+        denomination.push_back(temp_denomination);
         return dp[n][sum] = 1;
+    }
 
     // If number of coins is 0 or sum is less than 0 then
     // there is no way to make the sum.
@@ -16,10 +19,20 @@ int count(vector<int> &coins, int n, int sum, vector<vector<int>> &dp)
     // If the subproblem is previously calculated then
     // simply return the result
     if (dp[n][sum] != -1)
+    {
+        if (dp[n][sum] > 0)
+            denomination.push_back(temp_denomination);
         return dp[n][sum];
+    }
 
     // Two options for the current coin
-    return dp[n][sum] = count(coins, n, sum - coins[n - 1], dp) + count(coins, n - 1, sum, dp);
+    temp_denomination.push_back(coins[n - 1]);
+    int include = count(coins, n, sum - coins[n - 1], dp, denomination, temp_denomination);
+
+    temp_denomination.pop_back();
+    int exclude = count(coins, n - 1, sum, dp, denomination, temp_denomination);
+
+    return dp[n][sum] = include + exclude;
 }
 
 tuple<int, vector<int>> readFile(string filepath)
@@ -54,31 +67,126 @@ tuple<int, vector<int>> readFile(string filepath)
     return make_tuple(amount, coins);
 }
 
+void displayDenomination(vector<vector<int>> &denomination)
+{
+    printf("Denomination: ");
+    for (int i = 0; i < denomination.size(); i++)
+    {
+        printf("(");
+        for (int j = 0; j < denomination[i].size(); j++)
+        {
+            printf("%d", denomination[i][j]);
+            if (j < denomination[i].size() - 1)
+                printf(", ");
+        }
+        printf(")");
+        if (i < denomination.size() - 1)
+            printf(", ");
+    }
+
+    printf("\n");
+}
+
+void displayTime(chrono::nanoseconds duration)
+{
+    printf("Time taken: ");
+    if (duration.count() < 1000)
+        printf("%'ld ns\n", duration.count());
+    else if (duration.count() < 1000000)
+        printf("%'ld \u03BCs\n", chrono::duration_cast<chrono::microseconds>(duration).count());
+    else if (duration.count() < 1000000000)
+        printf("%'ld ms\n", chrono::duration_cast<chrono::milliseconds>(duration).count());
+    else
+        printf("%'ld s\n", chrono::duration_cast<chrono::seconds>(duration).count());
+}
+
+void displayTable(vector<vector<int>> &dp, vector<int> &coins)
+{
+    int n = dp.size();
+    int sum = dp[0].size();
+
+    // Display the table header
+    for (int i = -1; i < sum; i++)
+    {
+        printf("%3d ", i);
+    }
+    printf("\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0)
+            printf("%3d ", coins[i - 1]);
+        else
+            printf("%3d ", 0);
+
+        for (int j = 0; j < sum; j++)
+        {
+            // if (dp[i][j] <= 0)
+            //     printf("%3s ", " ");
+            // else
+            printf("%3d ", dp[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+vector<string> getAllTestCaseFile(string folderPath)
+{
+    vector<string> files;
+    for (const auto &entry : filesystem::directory_iterator(folderPath))
+    {
+        files.push_back(entry.path());
+    }
+
+    sort(files.begin(), files.end(), [](const string &a, const string &b)
+         { return stoi(a.substr(a.find_last_of("/\\") + 1).substr(2, 3)) > stoi(b.substr(b.find_last_of("/\\") + 1).substr(2, 3)); });
+
+    return files;
+}
+
 int main()
 {
 
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int amount;
-    vector<int> coins;
-    tie(amount, coins) = readFile("./test/4.14(Extra).txt");
+    vector<string> files = getAllTestCaseFile("./test");
+    // vector<string> files;
+    // files.push_back("./test/4.0.txt");
 
-    int n = coins.size();
+    while (!files.empty())
+    {
+        string filepath = files.back();
+        files.pop_back();
 
-    printf("Amount: %d\n", amount);
-    printf("Coins %d: ", n);
-    for (int i = 0; i < n; i++)
-        printf("%d ", coins[i]);
+        printf("File: %s\n", filepath.c_str());
 
-    printf("\n");
+        int amount;
+        vector<int> coins;
+        tie(amount, coins) = readFile(filepath);
 
-    vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
+        int n = coins.size();
 
-    auto start = chrono::high_resolution_clock::now();
-    printf("Number of ways: %'d\n", count(coins, n, amount, dp));
-    auto end = chrono::high_resolution_clock::now();
-    printf("Time taken: %'ld milliseconds\n", chrono::duration_cast<chrono::milliseconds>(end - start).count());
+        printf("Amount: %d\n", amount);
+        // printf("Coins: ");
+        // for (int i = 0; i < n; i++)
+        //     printf("%d ", coins[i]);
+        // printf("\n");
 
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
+        vector<vector<int>> denomination;
+        vector<int> temp_denomination;
+
+        auto start = chrono::high_resolution_clock::now();
+        printf("Number of ways: %'d\n", count(coins, n, amount, dp, denomination, temp_denomination));
+        auto end = chrono::high_resolution_clock::now();
+
+        // Display Denomination
+        // displayDenomination(denomination);
+        displayTime(end - start);
+        // displayTable(dp, coins);
+
+        printf("\n");
+    }
     return 0;
 }
