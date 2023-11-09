a07e87db0c4f6df46cca5013033d5446c62b7e95
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-09 18:16:33 +0700

    Add naive string matching algorithm implementation

---
+++ LAB9/LAB9_2.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Match
+{
+    int start;         // start position of match
+    char direction[2]; // LR or RL
+};
+
+vector<Match> naiveStringMatching(vector<char> pattern, vector<char> text)
+{
+    vector<Match> matches;
+    int patternSize = pattern.size();
+    int textSize = text.size();
+
+    for (int i = 0; i < textSize - patternSize + 1; i++)
+    {
+        bool match = true;
+        for (int j = 0; j < patternSize; j++)
+        {
+            if (text[i + j] != pattern[j])
+            {
+                match = false;
+                break;
+            }
+        }
+        if (match)
+        {
+            Match m;
+            m.start = i + 1;
+            m.direction[0] = 'L';
+            m.direction[1] = 'R';
+            matches.push_back(m);
+        }
+    }
+
+    reverse(pattern.begin(), pattern.end());
+
+    for (int i = 0; i < textSize - patternSize + 1; i++)
+    {
+        bool match = true;
+        for (int j = 0; j < patternSize; j++)
+        {
+            if (text[i + j] != pattern[j])
+            {
+                match = false;
+                break;
+            }
+        }
+        if (match)
+        {
+            Match m;
+            m.start = patternSize + i;
+            m.direction[0] = 'R';
+            m.direction[1] = 'L';
+            matches.push_back(m);
+        }
+    }
+
+    return matches;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    vector<char> chars;
+
+    // Get Line and split line with space push into vector chars
+    string line;
+    getline(cin, line);
+
+    stringstream ss(line);
+    char c;
+    while (ss >> c)
+    {
+        chars.push_back(c);
+    }
+
+    // Get pattern and text
+    int patternSize, textSize;
+
+    // Get pattern and text size
+    cin >> patternSize >> textSize;
+
+    vector<char> pattern(patternSize);
+    vector<char> text(textSize);
+
+    // Get pattern
+    for (int i = 0; i < patternSize; i++)
+        cin >> pattern[i];
+
+    // Get text
+    for (int i = 0; i < textSize; i++)
+        cin >> text[i];
+
+    // Naive String Matching
+    vector<Match> matches = naiveStringMatching(pattern, text);
+
+    cout << "Naive String Matching Don't have prefix function" << endl;
+
+    // Print result
+    cout << matches.size() << endl;
+
+    for (Match m : matches)
+        cout << m.start << " " << m.direction[0] << m.direction[1] << endl;
+
+    return 0;
+}
